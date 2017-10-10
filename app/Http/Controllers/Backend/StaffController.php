6e08a296cc97e28d35e25b0875a69b2616b38454
Thanh Hoa <hoaunet@yahoo.com>
2017-10-10 17:57:21 +0700

    test

--- app/Http/Controllers/Backend/StaffController.php
+++ app/Http/Controllers/Backend/StaffController.php
@@ -6,6 +6,7 @@ use Auth;
 use Hash;
 use App\User;
 use App\Http\Models\StaffModel;
+use App\Http\Models\BelongModel;
 use Form;
 use Html;
 use Input;
@@ -29,18 +30,72 @@ class StaffController extends BackendController
 		return view('backend.staff.index',$data);
 	}
 	public function getRegist(){
-		return view('backend.staff.regist');
+		$clsBelong = new BelongModel();
+		$divisions = $clsBelong->list_division_tree(); 
+		return view('backend.staff.regist', compact('divisions'));
 	}
+
+	public function postRegist()
+    {
+        $clsStaff      = new StaffModel();
+        $inputs         = Input::all();
+        $validator      = Validator::make($inputs, $clsStaff ->Rules(), $clsStaff ->Messages());
+        if ($validator->fails()) {
+            return redirect()->route('backend.staff.regist')->withErrors($validator)->withInput();
+        }
+        // insert
+        
+        $dataInsert             = array(
+            'staff_id_no'       => Input::get('staff_id_no'),
+            'staff_name'        => Input::get('staff_name'),           
+            'staff_belong'      => Input::get('staff_belong'),
+            'staff_card1'      => Input::get('staff_card1'),
+            'staff_card2'      => Input::get('staff_card2'),
+            'staff_card3'      => Input::get('staff_card3'),
+            'staff_card4'      => Input::get('staff_card4'),
+            'staff_card5'      => Input::get('staff_card5'),
+            'staff_card6'      => Input::get('staff_card6'),
+            'staff_card7'      => Input::get('staff_card7'),
+            'staff_card8'      => Input::get('staff_card8'),
+            'staff_card9'      => Input::get('staff_card9'),
+            'staff_card10'      => Input::get('staff_card10'),
+            'staff_pc1'         => Input::get('staff_pc1'),
+            'staff_pc2'         => Input::get('staff_pc2'),
+            'staff_pc3'         => Input::get('staff_pc3'),
+            'staff_pc4'         => Input::get('staff_pc4'),
+            'staff_pc5'         => Input::get('staff_pc5'),
+            'staff_pc6'         => Input::get('staff_pc6'),
+            'staff_pc7'         => Input::get('staff_pc7'),
+            'staff_pc8'         => Input::get('staff_pc8'),
+            'staff_pc9'         => Input::get('staff_pc9'),
+            'staff_pc10'         => Input::get('staff_pc10'),
+            'last_date'         => date('Y-m-d H:i:s'),
+            'last_kind'         => INSERT,
+            'last_ipadrs'       => CLIENT_IP_ADRS,
+            'last_user'         => Auth::user()->u_id            
+        );
+        
+        if ( $clsStaff ->insert($dataInsert) ) {
+            Session::flash('success', trans('common.msg_regist_success'));
+        } else {
+            Session::flash('danger', trans('common.msg_regist_danger'));
+        }
+        return redirect()->route('backend.staff.index');
+    }
+
 	public function import(){
 		return view('backend.staff.import');
 	}
+
 	public function search(){
 		return view('backend.staff.search');
 	}
+
 	public function getDelete()
 	{
 
 	}
+
 	public function getEdit()
 	{
 		
--- resources/views/backend/staff/regist.blade.php
+++ resources/views/backend/staff/regist.blade.php
@@ -20,13 +20,13 @@
             <div class="graph-form agile_info_shadow">
             <!--form-->
               <div class="form-body">
-                {!! Form::open(array('url' => 'division/regist', 'method' => 'post')) !!} 
+                {!! Form::open(array('url' => 'division/regist','id'=>'frmRegist', 'method' => 'post')) !!} 
                   <table class="table table-bordered mar-bottom15">
                     <tr>
                       <td class="col-title col-md-3"><label for="">社員番号</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_id_no" name="staff_id_no">
                         </div>
                       </td>
                     </tr>
@@ -34,7 +34,7 @@
                       <td class="col-title col-md-3"><label for="">社員名</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_name" name="staff_name">
                         </div>
                       </td>
                     </tr>
@@ -42,14 +42,29 @@
                       <td class="col-title col-md-3"><label for="">部署名</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <select name="select" class="form-control">
-                            <option>営業部</option>
-                            <option>├営業一課</option>
-                            <option>├営業二課</option>
-                            <option>└営業三課</option>
-                            <option>総務人事部</option>
-                            <option>├総務課</option>
-                            <option>└人事課</option>
+                          <select name="staff_belong" id="staff_belong" class="form-control">
+                             @if(!empty($divisions))
+                              @foreach($divisions as $division)
+
+                                  @if(!empty($division->belong_parent_id))
+                                    @if($division->belong_id == $division->belong_parent_id)
+                                      <option value="{{$division->belong_id}}" @if(old('u_belong') == $division->belong_id) selected @endif > - {{$division->belong_name}} </option>
+                                    @else
+                                    <option value="{{$division->belong_id}}" @if(old('u_belong') == $division->belong_id) selected @endif > &#12288;|- {{$division->belong_name}} </option>
+                                    @endif
+
+                                  @else
+                                    <option value="{{$division->belong_id}}" @if(old('u_belong') == $division->belong_id) selected @endif > {{$division->belong_name}} </option>
+                                  @endif
+
+                              @endforeach
+                            @endif
+                          </select>
+                          @if ($errors->has('u_belong'))
+                              <span class="help-block">
+                                  <strong>{{ $errors->first('u_belong') }}</strong>
+                              </span>
+                          @endif
                           </select>
                         </div>
                       </td>
@@ -58,7 +73,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(1)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card1" name="staff_card1">
                         </div>
                       </td>
                     </tr>
@@ -66,7 +81,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(2)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card2" name="staff_card2">
                         </div>
                       </td>
                     </tr>
@@ -74,7 +89,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(3)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card3" name="staff_card3">
                         </div>
                       </td>
                     </tr>
@@ -82,7 +97,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(4)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card4" name="staff_card4">
                         </div>
                       </td>
                     </tr>
@@ -90,7 +105,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(5)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card5" name="staff_card5">
                         </div>
                       </td>
                     </tr>
@@ -98,7 +113,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(6)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card6" name="staff_card6">
                         </div>
                       </td>
                     </tr>
@@ -106,7 +121,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(7)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card7" name="staff_card7">
                         </div>
                       </td>
                     </tr>
@@ -114,7 +129,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(8)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card8" name="staff_card8">
                         </div>
                       </td>
                     </tr>
@@ -122,7 +137,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(9)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card9" name="staff_card9">
                         </div>
                       </td>
                     </tr>
@@ -130,7 +145,7 @@
                       <td class="col-title col-md-3"><label for="">入退出カード番号(10)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_card10" name="staff_card10">
                         </div>
                       </td>
                     </tr>
@@ -138,7 +153,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(1)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc1" name="staff_pc1">
                         </div>
                       </td>
                     </tr>
@@ -146,7 +161,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(2)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc2" name="staff_pc2">
                         </div>
                       </td>
                     </tr>
@@ -154,7 +169,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(3)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc3" name="staff_pc3">
                         </div>
                       </td>
                     </tr>
@@ -162,7 +177,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(4)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc4" name="staff_pc4">
                         </div>
                       </td>
                     </tr>
@@ -170,7 +185,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(5)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc5" name="staff_pc5">
                         </div>
                       </td>
                     </tr>
@@ -178,7 +193,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(6)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc6" name="staff_pc6">
                         </div>
                       </td>
                     </tr>
@@ -186,7 +201,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(7)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc7" name="staff_pc7">
                         </div>
                       </td>
                     </tr>
@@ -194,7 +209,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(8)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc8" name="staff_pc8">
                         </div>
                       </td>
                     </tr>
@@ -202,7 +217,7 @@
                       <td class="col-title col-md-3"><label for="">PC番号(9)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc9" name="staff_pc9">
                         </div>
                       </td>
                     </tr>
@@ -210,14 +225,14 @@
                       <td class="col-title col-md-3"><label for="">PC番号(10)</label></td>
                       <td class="col-md-9">
                         <div class="col-md-6">
-                          <input type="text" class="form-control" id="">
+                          <input type="text" class="form-control" id="staff_pc10" name="staff_pc10">
                         </div>
                       </td>
                     </tr>
                   </table>
                   <div class="row">
                     <div class="col-md-12 text-center">
-                      <input onclick="location.href='staff_list.html'" value="登録する" type="button" class="btn btn-primary btn-sm">
+                      <input  value="登録する" type="button" name="btnSubmit" id="btnSubmit" class="btn btn-primary btn-sm">
                       <input name="button3" value="クリア" type="reset" class="btn btn-primary btn-sm mar-left15">
                     </div>
                   </div>
@@ -227,5 +242,10 @@
               <!--form-->
             </div>
           </div>     
-
+<script type="text/javascript">
+$("#btnSubmit").on("click",function() {
+   
+  $( "#frmRegist" ).submit(); 
+});
+</script>
 @endsection
