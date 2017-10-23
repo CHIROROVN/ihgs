4ff08d07224857f646f517ffad761c4c03fda11e
hoaunet <hoaunet@yahoo.com>
2017-10-23 16:05:21 +0700

    Update working time

--- app/Http/Models/WorkingTimeModel.php
+++ app/Http/Models/WorkingTimeModel.php
@@ -22,7 +22,14 @@ class WorkingTimeModel
     public function get_all($belong_id=null, $year=null)
     {        
         $results = DB::table($this->table)->join('m_belong', 't_staff.staff_belong', '=', 'm_belong.belong_id')->where('t_staff.last_kind', '<>', DELETE);
-        if(!empty($belong_id))     $results = $results->where('staff_belong',  '=', $belong_id);               
+        //if(!empty($belong_id))     $results = $results->where('staff_belong',  '=', $belong_id);     
+        if(!empty($belong_id)){
+              $results = $results->Where(function ($query) use ($belong_id) {
+                                                            $query->where('m_belong.belong_id',  '=', $belong_id)
+                                                                  ->orWhere('m_belong.belong_parent_id','=', $belong_id);
+                                                        });
+                   
+        }             
 
         $count = $results->count();
         $data  = $results->orderBy('staff_id', 'desc')->get();       
