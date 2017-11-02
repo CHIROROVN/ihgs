0d66e054cb4e1c7ffa4725fea492745f1e3c7160
hoaunet <hoaunet@yahoo.com>
2017-11-02 14:23:23 +0700

    update

--- app/Http/Controllers/Backend/SectionController.php
+++ app/Http/Controllers/Backend/SectionController.php
@@ -97,12 +97,12 @@ class SectionController extends BackendController
         $inputs         = Input::all();
         $validator      = Validator::make($inputs, $clsBelong->Rules(), $clsBelong->Messages());
         if ($validator->fails()) {
-            return redirect()->route('backend.section.edit', [$id])->withErrors($validator)->withInput();
+            return redirect()->route('backend.section.edit', [$parent_id,$id])->withErrors($validator)->withInput();
         }
         $belong = $clsBelong->get_by_belong_code(Input::get('belong_code'));
         if(isset($belong->belong_id) && $belong->belong_id !=$id){
             $error['belong_code']      = trans('validation.error_section_code_unique');  
-            return redirect()->route('backend.section.edit', [$id])->withErrors($error)->withInput();
+            return redirect()->route('backend.section.edit', [$parent_id,$id])->withErrors($error)->withInput();
         }
         // update
         $dataUpdate = array(
